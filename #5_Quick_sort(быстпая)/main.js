13ccb265fc2b9e3fe279becc05c6e280486fa395
Vovan1608 <vovan1608@gmail.com>
2021-03-20 13:32:40 +0200

    refactor

--- #5_Quick_sort(быстпая)/main.js
+++ #5_Quick_sort(быстпая)/main.js
@@ -12,91 +12,87 @@
 */
 
 // этап разделения:
-// Здесь мы берем последний элемент в качестве стержня. Мы используем 
-// переменную pivotIndex, чтобы отслеживать «среднее» положение, когда 
+// Здесь мы берем последний элемент в качестве стержня. Мы используем
+// переменную pivotIndex, чтобы отслеживать «среднее» положение, когда
 // все элементы слева меньше, а все элементы справа больше, чем pivotValue.
-// На последнем этапе мы меняем местами сводную точку, которая в нашем 
-// случае является последним элементом, на pivotIndex. Итак, в конце концов, 
-// наш опорный элемент окажется в «середине». Со всеми элементами, меньшими, 
-// чем точка поворота, слева от нее, и всеми элементами, которые больше или 
+// На последнем этапе мы меняем местами сводную точку, которая в нашем
+// случае является последним элементом, на pivotIndex. Итак, в конце концов,
+// наш опорный элемент окажется в «середине». Со всеми элементами, меньшими,
+// чем точка поворота, слева от нее, и всеми элементами, которые больше или
 // равны оси поворота справа от нее.
 
-function partition(arr, start, end){
-	// Берем последний элемент за pivot
-	const pivotValue = arr[end];
-	let pivotIndex = start; 
-	
-	for (let i = start; i < end; i++) {
-		
-		if (arr[i] < pivotValue) {
-		// меняем элементы местами
-		[arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
-		// идем к следующему элементу
-		pivotIndex++;
-		}
-	}
-	
-	// Putting the pivot value in the middle
-	[arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]] 
-	return pivotIndex;
-};
+function partition(arr, start, end) {
+  // Берем последний элемент за pivot
+  const pivotValue = arr[end];
+  let pivotIndex = start;
 
+  for (let i = start; i < end; i++) {
+    if (arr[i] < pivotValue) {
+      // меняем элементы местами
+      [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+      // идем к следующему элементу
+      pivotIndex++;
+    }
+  }
 
-// В этой функции мы начинаем с разбиения массива. После этого мы 
-// разделяем левый и правый подмассивы. Мы повторяем этот процесс 
-// до тех пор, пока метод не получит массив, который не пуст или 
+  [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
+
+  return pivotIndex;
+}
+
+// В этой функции мы начинаем с разбиения массива. После этого мы
+// разделяем левый и правый подмассивы. Мы повторяем этот процесс
+// до тех пор, пока метод не получит массив, который не пуст или
 // имеет более одного элемента.
 // Это связано с тем, что пустые массивы и массивы только с одним
 // элементом считаются отсортированными.
 
 function quickSortRecursive(arr, start, end) {
-	// Base case or terminating case
-	if (start >= end) {
-		return;
-	}
-	
-	// Returns pivotIndex
-	let index = partition(arr, start, end);
-	
-	// Recursively apply the same logic to the left and right subarrays
-	quickSortRecursive(arr, start, index - 1);
-	quickSortRecursive(arr, index + 1, end);
+  // Base case or terminating case
+  if (start >= end) {
+    return;
+  }
+
+  // Returns pivotIndex
+  let index = partition(arr, start, end);
+
+  // Recursively apply the same logic to the left and right subarrays
+  quickSortRecursive(arr, start, index - 1);
+  quickSortRecursive(arr, index + 1, end);
 }
 
-
-// мтеррационный вариант
+// итеррационный вариант
 function quickSortIterative(arr) {
-	// Creating an array that we'll use as a stack, using the push() and pop() functions
-	stack = [];
-	
-	// Adding the entire initial array as an "unsorted subarray"
-	stack.push(0);
-	stack.push(arr.length - 1);
-	
-	// There isn't an explicit peek() function
-	// The loop repeats as long as we have unsorted subarrays
-	while(stack[stack.length - 1] >= 0){
-			
-		// Extracting the top unsorted subarray
-		end = stack.pop();
-		start = stack.pop();
-			
-		pivotIndex = partition(arr, start, end);
-			
-		// If there are unsorted elements to the "left" of the pivot,
-		// we add that subarray to the stack so we can sort it later
-		if (pivotIndex - 1 > start){
-			stack.push(start);
-			stack.push(pivotIndex - 1);
-		}
-		
-		// If there are unsorted elements to the "right" of the pivot,
-		// we add that subarray to the stack so we can sort it later
-		if (pivotIndex + 1 < end){
-			stack.push(pivotIndex + 1);
-			stack.push(end);
-		}
-	}
+  // Creating an array that we'll use as a stack, using the push() and pop() functions
+  stack = [];
+
+  // Adding the entire initial array as an "unsorted subarray"
+  stack.push(0);
+  stack.push(arr.length - 1);
+
+  // There isn't an explicit peek() function
+  // The loop repeats as long as we have unsorted subarrays
+  while (stack[stack.length - 1] >= 0) {
+    // Extracting the top unsorted subarray
+    end = stack.pop();
+    start = stack.pop();
+
+    pivotIndex = partition(arr, start, end);
+
+    // If there are unsorted elements to the "left" of the pivot,
+    // we add that subarray to the stack so we can sort it later
+    if (pivotIndex - 1 > start) {
+      stack.push(start);
+      stack.push(pivotIndex - 1);
+    }
+
+    // If there are unsorted elements to the "right" of the pivot,
+    // we add that subarray to the stack so we can sort it later
+    if (pivotIndex + 1 < end) {
+      stack.push(pivotIndex + 1);
+      stack.push(end);
+    }
+  }
 }
 
 const array = [7, -2, 4, 1, 6, 5, 0, -4, 2];
